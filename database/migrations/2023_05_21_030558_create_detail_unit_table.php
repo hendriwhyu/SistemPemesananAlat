f1c88178f7166221b766d2c39471f44261e6df87
hendriwhyu <hendriwp123@gmail.com>
2023-05-21 10:30:36 +0700

    makeDetail&Foreign

---
+++ database/migrations/2023_05_21_030558_create_detail_unit_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_unit', function (Blueprint $table) {
+            $table->string('kode_alat');
+            $table->foreign('kode_alat')->references('kode_alat')->on('alatberat');
+            $table->string('harga');
+            $table->string('deskripsi');
+            $table->enum('type_book', ['jam', 'hari']);
+            $table->text('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_unit');
+    }
+};
