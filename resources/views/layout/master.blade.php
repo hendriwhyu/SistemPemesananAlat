67661925d708cf3a3c30a3e811bde75d1cc0a4d3
hendriwhyu <hendriwp123@gmail.com>
2023-05-22 19:10:07 +0700

    userexperience

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -20,6 +20,7 @@
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <!-- App CSS -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('template/assets/images/icon.png') }}">
     <link id="theme-style" rel="stylesheet" href="{{ asset('template/assets/css/portal.css') }}">
 
 </head>
